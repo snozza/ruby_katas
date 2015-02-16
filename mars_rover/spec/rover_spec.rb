3eed28850231108ad61d2611738766e92420280c
Andrew Snead <andrewcsnead@gmail.com>
2015-02-16 17:52:21 +0000

    added test for turning when facing north

--- mars_rover/spec/rover_spec.rb
+++ mars_rover/spec/rover_spec.rb
@@ -22,14 +22,19 @@ describe Rover do
 
     let(:rover) { Rover.new }
 
-    it('can move one forward') do
+    it('can move one forward facing north') do
       rover.move(['f'])
       expect(rover.position).to eql [1, 0]
     end
 
-    it('can move one forward one backward') do
+    it('can move one forward one backward facing north') do
       rover.move(['f', 'b'])
       expect(rover.position).to eql [0, 0]
+    end
+
+    it('can move one forward one backward turn left facing north') do
+      rover.move(['f', 'l', 'b'])
+      expect(rover.position).to eql [1, 1]
     end
 
     it('can move two forward two backward facing south') do
